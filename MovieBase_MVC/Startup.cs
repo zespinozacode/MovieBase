7e9ad1c5f60adc75d4184452509744e7bcd66af0
Zachary Espinoza <zespinozacode@gmail.com>
2018-12-05 03:47:54 -0500

    Add project files.

---
+++ MovieBase_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieBase_MVC.Startup))]
+namespace MovieBase_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
