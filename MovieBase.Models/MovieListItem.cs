d10bd089fd6cd73a21c76189012010705c677824
Zachary Espinoza <zacharyespinoza1@gmail.com>
2018-12-11 12:10:47 -0500

    frontend commit

--- MovieBase.Models/MovieListItem.cs
+++ MovieBase.Models/MovieListItem.cs
@@ -22,16 +22,18 @@ namespace MovieBase.Models
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name ="Release Date")]
         public DateTime ReleaseDate { get; set; }
 
         public string Description { get; set; }
 
-        [Display(Name = "Box Art")]
+        [Display(Name = "Cover Art")]
         public string CoverPicture { get; set; }
 
         [Display(Name = "Trailer")]
         public string TrailerLink { get; set; }
 
+        [Display(Name = "Genre")]
         public Genre GenreName { get; set; }
 
         public override string ToString() => Title;
